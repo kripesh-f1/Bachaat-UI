3a85a1125776ba2ad143118f25cd0db22e3defe7
rowansiwakoti <rowansiwakoti@gmail.com>
2018-05-24 18:23:03 +0545

    Create input-field-email-component with validations and write unit test

---
+++ src/app/shared-utils/form-utils/input-field-email/input-field-email.component.spec.ts
@@ -0,0 +1,56 @@
+import {async, ComponentFixture, TestBed} from '@angular/core/testing';
+import {FormsModule} from '@angular/forms';
+
+import {InputFieldEmailComponent} from './input-field-email.component';
+
+describe('InputFieldEmailComponent', () => {
+  let component: InputFieldEmailComponent;
+  let fixture: ComponentFixture<InputFieldEmailComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      imports: [FormsModule],
+      declarations: [InputFieldEmailComponent]
+    })
+      .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(InputFieldEmailComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should initialize the component', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should define input element ', () => {
+    const element = fixture.debugElement.nativeElement;
+    expect(element.querySelector('input')).toBeDefined();
+  });
+
+  it('should enter an email address', () => {
+    const element = fixture.debugElement.nativeElement;
+    element.querySelector('input').value = 'rowansiwakoti@gmail.com';
+    fixture.detectChanges();
+    expect(element.querySelector('input').value).toEqual('rowansiwakoti@gmail.com');
+  });
+
+  it('should enter an invalid email address', () => {
+    const element = fixture.debugElement.nativeElement;
+    element.querySelector('input').value = 'rowansiwakoti@';
+    fixture.detectChanges();
+    const div = element.querySelector('.form-group .alert').children[1];
+    expect(div.innerHTML.includes('email@example.com')).toBeTruthy();
+  });
+
+  it('should show required message if the input field is empty', () => {
+    const element = fixture.debugElement.nativeElement;
+    element.querySelector('input').value = '';
+    fixture.detectChanges();
+    const div = element.querySelector('.form-group .alert').children[0];
+    expect(div.innerHTML.includes('Email is required')).toBeTruthy();
+  });
+
+});
---
+++ src/app/shared-utils/form-utils/input-field-email/input-field-email.component.ts
@@ -0,0 +1,65 @@
+import {Component, forwardRef, Input, OnInit} from '@angular/core';
+import {ControlValueAccessor, NG_VALUE_ACCESSOR} from '@angular/forms';
+
+const noop = () => {
+};
+
+export const CUSTOM_INPUT_CONTROL_VALUE_ACCESSOR: any = {
+  provide: NG_VALUE_ACCESSOR,
+  useExisting: forwardRef(() => InputFieldEmailComponent),
+  multi: true
+};
+
+@Component({
+  selector: 'app-input-field-email',
+  templateUrl: './input-field-email.component.html',
+  styleUrls: ['./input-field-email.component.css'],
+  providers: [CUSTOM_INPUT_CONTROL_VALUE_ACCESSOR]
+})
+export class InputFieldEmailComponent implements OnInit, ControlValueAccessor {
+  @Input() isRequired: boolean;
+
+  // The internal data model
+  private innerValue: any = '';
+
+  // Placeholders for the callbacks which are later provided
+  // by the Control Value Accessor
+  private onTouchedCallback: () => void = noop;
+  private onChangeCallback: (_: any) => void = noop;
+
+  constructor() {
+  }
+
+  ngOnInit() {
+  }
+
+  // get accessor
+  get value(): any {
+    return this.innerValue;
+  }
+
+  // set accessor including call the onchange callback
+  set value(v: any) {
+    if (v !== this.innerValue) {
+      this.innerValue = v;
+      this.onChangeCallback(v);
+    }
+  }
+
+  // From ControlValueAccessor interface
+  writeValue(value: any) {
+    if (value !== this.innerValue) {
+      this.innerValue = value;
+    }
+  }
+
+  // From ControlValueAccessor interface
+  registerOnChange(fn: any) {
+    this.onChangeCallback = fn;
+  }
+
+  // From ControlValueAccessor interface
+  registerOnTouched(fn: any) {
+    this.onTouchedCallback = fn;
+  }
+}
