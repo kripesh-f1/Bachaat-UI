4433d8fec9491d7998fd68e7a8421eca354430e2
rowansiwakoti <rowansiwakoti@gmail.com>
2018-05-25 16:36:23 +0545

    Revert app component files

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,16 +1,10 @@
-import {TestBed, async} from '@angular/core/testing';
-import {FormsModule} from '@angular/forms';
-
-import {AppComponent} from './app.component';
-import {InputFieldEmailComponent} from './shared-utils/form-utils/input-field-email/input-field-email.component';
-
+import { TestBed, async } from '@angular/core/testing';
+import { AppComponent } from './app.component';
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      imports: [FormsModule],
       declarations: [
-        AppComponent,
-        InputFieldEmailComponent
+        AppComponent
       ],
     }).compileComponents();
   }));
@@ -18,5 +12,16 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
+  }));
+  it(`should have as title 'app'`, async(() => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.debugElement.componentInstance;
+    expect(app.title).toEqual('app');
+  }));
+  it('should render title in a h1 tag', async(() => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to app!');
   }));
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,9 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-
-  onSubmit(value) {
-    console.log(value);
-  }
-
-  }
+  title = 'app';
+}
