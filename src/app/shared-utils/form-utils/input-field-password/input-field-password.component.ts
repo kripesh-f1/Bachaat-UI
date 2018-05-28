1ee57c9d028e94b10b0815879bdfec8db8d516b8
rowansiwakoti <rowansiwakoti@gmail.com>
2018-05-28 19:00:59 +0545

    Implement custom value accessor

--- src/app/shared-utils/form-utils/input-field-password/input-field-password.component.ts
+++ src/app/shared-utils/form-utils/input-field-password/input-field-password.component.ts
@@ -1,20 +1,68 @@
-import { Component, OnInit, Input } from '@angular/core';
+import {Component, forwardRef, Input, OnInit} from '@angular/core';
+import {ControlValueAccessor, NG_VALUE_ACCESSOR} from '@angular/forms';
+
+const noop = () => {
+};
+
+export const CUSTOM_INPUT_CONTROL_VALUE_ACCESSOR: any = {
+  provide: NG_VALUE_ACCESSOR,
+  useExisting: forwardRef(() => InputFieldPasswordComponent),
+  multi: true
+};
 
 @Component({
   selector: 'app-input-field-password',
   templateUrl: './input-field-password.component.html',
-  styleUrls: ['./input-field-password.component.css']
+  styleUrls: ['./input-field-password.component.css'],
+  providers: [CUSTOM_INPUT_CONTROL_VALUE_ACCESSOR]
 })
-export class InputFieldPasswordComponent implements OnInit {
+export class InputFieldPasswordComponent implements OnInit, ControlValueAccessor {
   @Input() isRequired: boolean;
   @Input() minLength: number;
   @Input() maxLength: number;
-  value: string;
+  // The internal data model
+  private innerValue: any = '';
+
+  // Placeholders for the callbacks which are later provided
+  // by the Control Value Accessor
+  private onTouchedCallback: () => void = noop;
+  private onChangeCallback: (_: any) => void = noop;
+
   constructor() {
     this.isRequired = true;
   }
 
   ngOnInit() {
+  }
+
+  // get accessor
+  get value(): any {
+    return this.innerValue;
+  }
+
+  // set accessor including call the onchange callback
+  set value(v: any) {
+    if (v !== this.innerValue) {
+      this.innerValue = v;
+      this.onChangeCallback(v);
+    }
+  }
+
+  // From ControlValueAccessor interface
+  writeValue(value: any) {
+    if (value !== this.innerValue) {
+      this.innerValue = value;
+    }
+  }
+
+  // From ControlValueAccessor interface
+  registerOnChange(fn: any) {
+    this.onChangeCallback = fn;
+  }
+
+  // From ControlValueAccessor interface
+  registerOnTouched(fn: any) {
+    this.onTouchedCallback = fn;
   }
 
 }
--- src/app/shared-utils/form-utils/input-field-text/input-field-text.component.ts
+++ src/app/shared-utils/form-utils/input-field-text/input-field-text.component.ts
@@ -1,19 +1,70 @@
-import { Component, OnInit, Input } from '@angular/core';
+import {Component, forwardRef, Input, OnInit} from '@angular/core';
+import {ControlValueAccessor, NG_VALUE_ACCESSOR} from '@angular/forms';
+
+const noop = () => {
+};
+
+export const CUSTOM_INPUT_CONTROL_VALUE_ACCESSOR: any = {
+  provide: NG_VALUE_ACCESSOR,
+  useExisting: forwardRef(() => InputFieldTextComponent),
+  multi: true
+};
 
 @Component({
   selector: 'app-input-field-text',
   templateUrl: './input-field-text.component.html',
-  styleUrls: ['./input-field-text.component.css']
+  styleUrls: ['./input-field-text.component.css'],
+  providers: [CUSTOM_INPUT_CONTROL_VALUE_ACCESSOR]
 })
-export class InputFieldTextComponent implements OnInit {
+export class InputFieldTextComponent implements OnInit, ControlValueAccessor {
   @Input() labelName: string;
   @Input() inputType: string;
   @Input() isRequired?: boolean;
   @Input() minLength?: number;
   @Input() maxLength?: number;
+
+  // The internal data model
+  private innerValue: any = '';
+
+  // Placeholders for the callbacks which are later provided
+  // by the Control Value Accessor
+  private onTouchedCallback: () => void = noop;
+  private onChangeCallback: (_: any) => void = noop;
+
   constructor() {
     this.isRequired = true;
   }
+
   ngOnInit() {
+  }
+
+  // get accessor
+  get value(): any {
+    return this.innerValue;
+  }
+
+  // set accessor including call the onchange callback
+  set value(v: any) {
+    if (v !== this.innerValue) {
+      this.innerValue = v;
+      this.onChangeCallback(v);
+    }
+  }
+
+  // From ControlValueAccessor interface
+  writeValue(value: any) {
+    if (value !== this.innerValue) {
+      this.innerValue = value;
+    }
+  }
+
+  // From ControlValueAccessor interface
+  registerOnChange(fn: any) {
+    this.onChangeCallback = fn;
+  }
+
+  // From ControlValueAccessor interface
+  registerOnTouched(fn: any) {
+    this.onTouchedCallback = fn;
   }
 }
