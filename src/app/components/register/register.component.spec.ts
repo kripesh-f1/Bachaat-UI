df88d2d4f22938553285273b9a1ae8953c009a73
rowansiwakoti <rowansiwakoti@gmail.com>
2018-06-01 16:06:09 +0545

    Resolve unit test conflicts

--- src/app/components/register/register.component.spec.ts
+++ src/app/components/register/register.component.spec.ts
@@ -1,6 +1,12 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import {async, ComponentFixture, TestBed} from '@angular/core/testing';
+import {FormsModule} from '@angular/forms';
 
-import { RegisterComponent } from './register.component';
+import {RegisterComponent} from './register.component';
+import {LabelComponent} from '../../shared-utils/form-utils/label/label.component';
+import {InputFieldTextComponent} from '../../shared-utils/form-utils/input-field-text/input-field-text.component';
+import {InputFieldPasswordComponent} from '../../shared-utils/form-utils/input-field-password/input-field-password.component';
+import {InputFieldEmailComponent} from '../../shared-utils/form-utils/input-field-email/input-field-email.component';
+import {ButtonComponent} from '../../shared-utils/form-utils/button/button.component';
 
 describe('RegisterComponent', () => {
   let component: RegisterComponent;
@@ -8,9 +14,17 @@ describe('RegisterComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ RegisterComponent ]
+      imports: [FormsModule],
+      declarations: [
+        RegisterComponent,
+        LabelComponent,
+        InputFieldTextComponent,
+        InputFieldPasswordComponent,
+        InputFieldEmailComponent,
+        ButtonComponent
+      ]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
