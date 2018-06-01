fd38de8b20f1b210d4a888f380dd7e931b0d71c1
rowansiwakoti <rowansiwakoti@gmail.com>
2018-06-01 16:10:29 +0545

    Resolve unit test conflicts

--- src/app/components/login/login.component.spec.ts
+++ src/app/components/login/login.component.spec.ts
@@ -1,6 +1,11 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import {async, ComponentFixture, TestBed} from '@angular/core/testing';
+import {FormsModule} from '@angular/forms';
 
-import { LoginComponent } from './login.component';
+import {LoginComponent} from './login.component';
+import {LabelComponent} from '../../shared-utils/form-utils/label/label.component';
+import {InputFieldTextComponent} from '../../shared-utils/form-utils/input-field-text/input-field-text.component';
+import {InputFieldPasswordComponent} from '../../shared-utils/form-utils/input-field-password/input-field-password.component';
+import {ButtonComponent} from '../../shared-utils/form-utils/button/button.component';
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
@@ -8,9 +13,16 @@ describe('LoginComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ LoginComponent ]
+      imports: [FormsModule],
+      declarations: [
+        LoginComponent,
+        LabelComponent,
+        InputFieldTextComponent,
+        InputFieldPasswordComponent,
+        ButtonComponent
+      ]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
