685696234816e5dda69461c8d653b386974c06c6
rowansiwakoti <rowansiwakoti@gmail.com>
2018-06-01 16:39:00 +0545

    Remove one unnecessary router module imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,6 @@ import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 import {RouterModule} from '@angular/router';
 import {FormsModule} from '@angular/forms';
-import {RouterModule} from '@angular/router';
 import {HttpClientModule} from '@angular/common/http';
 
 import {AppComponent} from './app.component';
